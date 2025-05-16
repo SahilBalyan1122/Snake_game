64be9be7ad1850614c1396952299d9a98aad32d7
Sahil <sahil.balyan@amantyatech.com>
2025-05-16 22:40:50 +0530

    highscore and other improvement

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ from snake import Snake
 from scorecard import ScoreCard
 from food import Food
 from game import Game
-
+import time
+run=True
 screen = Screen()
-snake = Snake()
-scorecard = ScoreCard()
-food = Food()
-game = Game(screen,snake,food,scorecard)
-game.setup_screen()
-snake.game = game
-game.move()
+def reload():
+    screen.clear()
+    snake = Snake()
+    scorecard = ScoreCard()
+    food = Food()
+    game = Game(screen,snake,food,scorecard)
+    game.setup_screen()
+    snake.game = game
+    game.move()
+    screen.onkey(reload, "r")
+
+reload()
+
 
 screen.exitonclick()
--- scorecard.py
+++ scorecard.py
@@ -1,23 +1,38 @@
 from turtle import Turtle
+from variables import HIGH_SCORE_FILE
 
 class ScoreCard(Turtle):
     def __init__(self):
         super().__init__()
         self.color("white")
         self.penup()
-        self.goto(0,280)
         self.hideturtle()
         self.score = 0
-        self.show_text("Score = "+str(self.score))
+        self.show_text("Score = "+str(self.score),(0,280))
+        self.high_score = self.load_high_score()
 
-    def show_text(self,string):
+    def show_text(self,string,pos=(0,0)):
+        self.goto(pos)
         self.write(string, False, "center", ('Arial', 12, 'bold'))
+
+    def load_high_score(self):
+        try:
+            with open(HIGH_SCORE_FILE, "r") as file:
+                return int(file.read())
+        except (FileNotFoundError, ValueError):
+            return 0
+
+    def save_high_score(self):
+        if self.score > self.high_score:
+            with open(HIGH_SCORE_FILE, "w") as file:
+                file.write(str(self.score))
+            self.show_text(f"Congratulations! You beat the previous High Score of {self.high_score}", (0, -40))
 
     def update_score(self):
         self.score += 1
         self.clear()
-        self.show_text("Score = "+str(self.score))
+        self.show_text("Score = "+str(self.score),(0,280))
 
     def game_over(self):
-        self.goto(0,0)
         self.show_text("GAME OVER")
+        self.save_high_score()
